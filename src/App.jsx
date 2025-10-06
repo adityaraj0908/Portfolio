00059ba47246f3e34d1553a3c90d1e5351ee239e
Aditya Raj <adityaraj1389@gmail.com>
2025-10-07 04:14:33 +0530

    Project initialisation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = ()=>
+{
+    return(
+        <main>
+            <div>
+                <h1>Welcome to my 3D portfolio !</h1>
+            </div>
+        </main>
+    )
+}
+
+
+
+export default App
